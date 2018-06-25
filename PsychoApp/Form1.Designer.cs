cd95b0e238e65c0ce7b05a44a1e0b4b102850d00
kolyasik <kolyanupasik@gmail.com>
2018-06-25 20:51:49 +0300

    25.06 commit

--- PsychoApp/Form1.Designer.cs
+++ PsychoApp/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(27, 156);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(148, 55);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "вопрос №1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(227, 156);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(148, 55);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.Text = "вопрос №2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(400, 253);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Name = "Form1";
+            this.Text = "Психодиагностика";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- PsychoApp/Form1.cs
+++ PsychoApp/Form1.cs
@@ -16,5 +16,224 @@ namespace PsychoApp
         {
             InitializeComponent();
         }
+
+        public string Name_psycho { get; set; }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            if (btn_1.Text == "вопрос №1")
+            {
+                Name_psycho = Name_psycho + "P";
+                btn_1.Text = "вопрос №3";
+                btn_2.Text = "вопрос №4";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_1.Text == "вопрос №3")
+            {
+                Name_psycho = Name_psycho + "N";
+                btn_1.Text = "вопрос №7";
+                btn_2.Text = "вопрос №8";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_1.Text == "вопрос №7")
+            {
+                Name_psycho = Name_psycho + "E";
+                btn_1.Text = "вопрос №15";
+                btn_2.Text = "вопрос №16";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_1.Text == "вопрос №9")
+            {
+                Name_psycho = Name_psycho + "E";
+                btn_1.Text = "вопрос №19";
+                btn_2.Text = "вопрос №20";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_1.Text == "вопрос №5")
+            {
+                Name_psycho = Name_psycho + "T";
+                btn_1.Text = "вопрос №11";
+                btn_2.Text = "вопрос №12";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_1.Text == "вопрос №11")
+            {
+                Name_psycho = Name_psycho + "E";
+                btn_1.Text = "вопрос №23";
+                btn_2.Text = "вопрос №24";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_1.Text == "вопрос №13")
+            {
+                Name_psycho = Name_psycho + "E";
+                btn_1.Text = "вопрос №27";
+                btn_2.Text = "вопрос №28";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_1.Text == "вопрос №15")
+            {
+                Name_psycho = Name_psycho + "T";
+                Form2 newForm = new Form2();
+                newForm.Show();
+                //newForm.label1_Click
+            }
+            else if (btn_1.Text == "вопрос №17")
+            {
+                Name_psycho = Name_psycho + "T";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_1.Text == "вопрос №19")
+            {
+                Name_psycho = Name_psycho + "T";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_1.Text == "вопрос №21")
+            {
+                Name_psycho = Name_psycho + "T";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_1.Text == "вопрос №23")
+            {
+                Name_psycho = Name_psycho + "N";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_1.Text == "вопрос №25")
+            {
+                Name_psycho = Name_psycho + "N";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_1.Text == "вопрос №27")
+            {
+                Name_psycho = Name_psycho + "N";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_1.Text == "вопрос №29")
+            {
+                Name_psycho = Name_psycho + "N";
+                MessageBox.Show(Name_psycho,"Результат");
+            }
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            //string question4 = "вопрос №4";
+            if (btn_2.Text == "вопрос №2")
+            {
+                Name_psycho = Name_psycho + "J";
+                btn_1.Text = "вопрос №5";
+                btn_2.Text = "вопрос №6";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_2.Text == "вопрос №4")
+            {
+                Name_psycho = Name_psycho + "S";
+                btn_1.Text = "вопрос №9";
+                btn_2.Text = "вопрос №10";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_2.Text == "вопрос №8")
+            {
+                Name_psycho = Name_psycho + "I";
+                btn_1.Text = "вопрос №17";
+                btn_2.Text = "вопрос №18";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_2.Text == "вопрос №10")
+            {
+                Name_psycho = Name_psycho + "I";
+                btn_1.Text = "вопрос №21";
+                btn_2.Text = "вопрос №22";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_2.Text == "вопрос №6")
+            {
+                Name_psycho = Name_psycho + "F";
+                btn_1.Text = "вопрос №13";
+                btn_2.Text = "вопрос №14";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_2.Text == "вопрос №12")
+            {
+                Name_psycho = Name_psycho + "I";
+                btn_1.Text = "вопрос №25";
+                btn_2.Text = "вопрос №26";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_2.Text == "вопрос №14")
+            {
+                Name_psycho = Name_psycho + "I";
+                btn_1.Text = "вопрос №29";
+                btn_2.Text = "вопрос №30";
+                label1.Text = Name_psycho;
+            }
+            else if (btn_2.Text == "вопрос №16")
+            {
+                Name_psycho = Name_psycho + "F";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_2.Text == "вопрос №18")
+            {
+                Name_psycho = Name_psycho + "F";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_2.Text == "вопрос №20")
+            {
+                Name_psycho = Name_psycho + "F";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_2.Text == "вопрос №22")
+            {
+                Name_psycho = Name_psycho + "F";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_2.Text == "вопрос №24")
+            {
+                Name_psycho = Name_psycho + "S";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_2.Text == "вопрос №26")
+            {
+                Name_psycho = Name_psycho + "S";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_2.Text == "вопрос №28")
+            {
+                Name_psycho = Name_psycho + "S";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+            else if (btn_2.Text == "вопрос №30")
+            {
+                Name_psycho = Name_psycho + "S";
+                MessageBox.Show(Name_psycho, "Результат");
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Btn_3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
+
+
+//btn_2.Text = "вопрос №4
