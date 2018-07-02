6d4d51b9f4694ab23eaa4b5121a35a2b22266393
kolyasik <kolyanupasik@gmail.com>
2018-07-03 01:15:36 +0300

    03.07.1

--- PsychoApp/Form1.cs
+++ PsychoApp/Form1.cs
@@ -9,16 +9,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Office.Interop.Excel;
+using System.Text.RegularExpressions;
 
 namespace PsychoApp
 {
-
-
-
     public partial class Form1 : Form
     {
-        //private Microsoft.Office.Interop.Excel.Application excelapp;
-
+        public string Name_psycho { get; set; }
 
         public string GetString(int numberString, string filePath, Encoding encoding)
         {
@@ -36,97 +33,19 @@ namespace PsychoApp
         public Form1()
         {
             InitializeComponent();
-
             btn_1.Text = GetString(1, "questions.txt", Encoding.UTF8);
             btn_2.Text = GetString(2, "questions.txt", Encoding.Default);
             label1.Text = GetString(0, "questions.txt", Encoding.Default);
-
-
-
-            //label1.Text = lnamev1v2;
-
-            //var Q1 = 0;
-            //string nameQ1= "1";
-
-            //StreamReader reader = new StreamReader("questions.txt", Encoding.Default);
-
-            //if (Q1 == 0)
-            //{
-            //    nameQ1 = (reader.ReadLine());
-            //    Q1++;
-            //}
-            //label1.Text = nameQ1;
         }
-
-
-        public string Name_psycho { get; set; }
-
-        //private string lnamev1v2 = "name";
-        //private string lnamev3v4 = "name1";
-        //private string lnamev5v6 = "name2";
-        //private string lnamev7v8 = "name3";
-        //private string lnamev9v10 = "name4";
-        //private string lnamev11v12 = "name5";
-        //private string lnamev13v14 = "name6";
-        //private string lnamev15v16 = "name7";
-        //private string lnamev17v18 = "name8";
-        //private string lnamev19v20 = "name9";
-        //private string lnamev21v22 = "name10";
-        //private string lnamev23v24 = "name11";
-        //private string lnamev25v26 = "name12";
-        //private string lnamev27v28 = "name13";
-        //private string lnamev29v30 = "name14";
-        //private string v1 = "Предпочитаете действовать по ситуации, импровизировать. Легко меняете планы. Умеете проявить гибкость и использовать шанс";
-        //private string v2 = "Предпочитаете действовать по намеченному плану, независимо от ситуации. Необходимость менять планы на ходу вас напрягает. Ваши мнения и решения устойчивы";
-        //private string v3 = "У меня развитая интуиция. Умею уступить в мелочах сейчас, чтобы дробится цели позже. Весь мир - в будущем.";
-        //private string v4 = "Ценю то, что имею сейчас. Нужно жить сегодня, а не откладывать на потом";
-        //private string v5 = "Рассудок; холодный анализ; логика; объективный подход; трезвое мышление; непредвзятость; сдержанность.";
-        //private string v6 = "Чувства, эмоции; сопереживание людям; неравнодушие; сердечность.";
-        //private string v7 = "Общителен со всеми. Легко завожу знакомства и берусь за все.";
-        //private string v8 = "Общителен только с близкими. Душевный покой. Переживания в себе.";
-        //private string v9 = "Воля; преодоление трудностей; сила и красота.";
-        //private string v10 = "Ощущение момента; интересы близких; эстетика.";
-        //private string v11 = "Правильные действия.";
-        //private string v12 = "Анализ и размышление.";
-        //private string v13 = "Очень часто везде открыто выражаю эмоции.";
-        //private string v14 = "Стараюсь скрывать и сдерживать эмоции.";
-        //private string v15 = "Вдохновляющие идеи; начинания и теории.";
-        //private string v16 = "Интересные, талантливые люди, общение.";
-        //private string v17 = "Скептическое предвидение; логика важнее.";
-        //private string v18 = "Эмоциональное предчувствие; поэтичность.";
-        //private string v19 = "Логика действий это главное, чувства вторичны.";
-        //private string v20 = "Понимаю отношения между людьми. Знаю как ими манипулировать.";
-        //private string v21 = "Практичность; умелые руки; независимость.";
-        //private string v22 = "Теплое общение и эмоции. Тяга к удовольствиям.";
-        //private string v23 = "Быстрота и эффективность.";
-        //private string v24 = "Добросовестность дел; надежность, качество.";
-        //private string v25 = "Мягкий и демократичный исследователь.";
-        //private string v26 = "Практик с сильной волей.";
-        //private string v27 = "Хорошая интуиция, сопереживание, драматизм.";
-        //private string v28 = "Радость, эмоции, доброе настроение.";
-        //private string v29 = "Объективность. Предвидя последствия, стараюсь не допускать и погашать конфликты.";
-        //private string v30 = "Субъективность. Исходя из своего опыта, могу пойти на конфликт чтобы защитить своих.";
-
-
+                
         private void Btn_1_Click(object sender, EventArgs e)
         {
             if (btn_1.Text == GetString(1, "questions.txt", Encoding.Default))
             {
-                //var Q2 = 3;
-                //string nameQ2 = "1";
-                //StreamReader reader = new StreamReader("questions.txt", Encoding.Default);
-
-                //if (Q2 < 4)
-                //{
-                //    nameQ2 = reader.ReadLine();
-                //    Q2++;
-                //}
                 Name_psycho = Name_psycho + "P";
                 btn_1.Text = GetString(4, "questions.txt", Encoding.Default);
                 btn_2.Text = GetString(5, "questions.txt", Encoding.Default);
                 label1.Text = GetString(3, "questions.txt", Encoding.Default);
-
-
             }
             else if (btn_1.Text == GetString(4, "questions.txt", Encoding.Default))
             {
@@ -174,6 +93,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "T";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -185,6 +113,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "T";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -196,6 +133,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "T";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -207,6 +153,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "T";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -218,6 +173,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "N";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -229,6 +193,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "N";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -240,6 +213,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "N";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -251,6 +233,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "N";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -259,8 +250,7 @@ namespace PsychoApp
                 Close();
             }
         }
-
-
+        
         private void Btn_2_Click(object sender, EventArgs e)
         {
             if (btn_2.Text == GetString(2, "questions.txt", Encoding.Default))
@@ -316,6 +306,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "F";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -327,6 +326,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "F";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -338,6 +346,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "F";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -349,6 +366,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "F";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -360,6 +386,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "S";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -371,6 +406,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "S";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -382,6 +426,15 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "S";
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2
                 {
                     Owner = this
@@ -393,14 +446,29 @@ namespace PsychoApp
             {
                 Hide();
                 Name_psycho = Name_psycho + "S";
-                //MessageBox.Show(Name_psycho,"Результат");
+                Regex myReg0 = new Regex("E|I");
+                Regex myReg1 = new Regex("S|N");
+                Regex myReg2 = new Regex("T|F");
+                Regex myReg3 = new Regex("P|J");
+                Match match0 = myReg0.Match(Name_psycho);
+                Match match1 = myReg1.Match(Name_psycho);
+                Match match2 = myReg2.Match(Name_psycho);
+                Match match3 = myReg3.Match(Name_psycho);
+                Name_psycho = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
                 Form2 newForm = new Form2();
                 newForm.Owner = this;
                 newForm.ShowDialog();
                 Close();
-                //Name_psycho = newForm.Name_form2;
-
             }
+            //Regex myReg0 = new Regex("E|I");
+            //Regex myReg1 = new Regex("S|N");
+            //Regex myReg2 = new Regex("T|F");
+            //Regex myReg3 = new Regex("P|J");
+            //Match match0 = myReg0.Match(Name_psycho);
+            //Match match1 = myReg1.Match(Name_psycho);
+            //Match match2 = myReg2.Match(Name_psycho);
+            //Match match3 = myReg3.Match(Name_psycho);
+            //EndResult = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
         }
 
         private void Label1_Click(object sender, EventArgs e)
--- PsychoApp/Form2.cs
+++ PsychoApp/Form2.cs
@@ -7,15 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace PsychoApp
 {
     public partial class Form2 : Form
     {
 
+        //Form1 g = (Form1)Owner;
+        //Regex myReg0 = new Regex("E|I");
+        //Regex myReg1 = new Regex("S|N");
+        //Regex myReg2 = new Regex("T|F");
+        //Regex myReg3 = new Regex("P|J");
+        //Match match0 = myReg0.Match(g.Name_psycho);
+        //Match match1 = myReg1.Match(g.Name_psycho);
+        //Match match2 = myReg2.Match(g.Name_psycho);
+        //Match match3 = myReg3.Match(g.Name_psycho);
+        //string YeahBoy = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
+
         public Form2()
         {
             InitializeComponent();
+            //g.Name_psycho
+
+
+            //string a = ((Form1)Owner).Name_psycho;
+            //Regex myReg0 = new Regex("E|I");
+            //Regex myReg1 = new Regex("S|N");
+            //Regex myReg2 = new Regex("T|F");
+            //Regex myReg3 = new Regex("P|J");
+            //Match match0 = myReg0.Match(a);
+            //Match match1 = myReg1.Match(a);
+            //Match match2 = myReg2.Match(a);
+            //Match match3 = myReg3.Match(a);
+            //string YeahBoy = string.Format("{0}{1}{2}{3}", match0, match1, match2, match3);
+            //textBox1.Text = YeahBoy;
+
+
         }
         
         private void Btn2_1_Click(object sender, EventArgs e)
@@ -42,6 +70,7 @@ namespace PsychoApp
             Form1 g = (Form1)Owner;
             label1.Text = g.Name_psycho;
             textBox1.Text = g.Name_psycho;
+
         }
     }
 }
