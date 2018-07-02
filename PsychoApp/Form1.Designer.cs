af132dbe42b8b465787791866ee78e0c9dd88204
nikolay larin <kolyanupasik@gmail.com>
2018-07-02 17:21:11 +0300

    02.0722

--- PsychoApp/Form1.Designer.cs
+++ PsychoApp/Form1.Designer.cs
@@ -62,7 +62,7 @@ namespace PsychoApp
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Sitka Text", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(213, 82);
+            this.label1.Location = new System.Drawing.Point(300, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 25);
             this.label1.TabIndex = 2;
