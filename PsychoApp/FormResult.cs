c50486eb3f6ae73b1635b41fffbce8604471a8ca
kolyasik <kolyanupasik@gmail.com>
2018-07-13 07:57:49 +0300

    13.07 commit

--- PsychoApp/FormResult.cs
+++ PsychoApp/FormResult.cs
@@ -32,78 +32,6 @@ namespace PsychoApp
           
             if (MessageBox.Show("Вы уверены?", "Выйти?", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
             {
-
-
-
-
-                ////    Excel.Application exApp = new Excel.Application();
-                ////    exApp.Visible = false;
-                ////    exApp.Workbooks.Add();
-                ////    Worksheet workSheet = (Worksheet)exApp.ActiveSheet;
-                ////    workSheet.Cells[1, 1] = "№";
-                ////    workSheet.Cells[1, 2] = "Имя студента";
-                ////    workSheet.Cells[1, 3] = "Группа";
-                ////    workSheet.Cells[1, 4] = "Средний балл";
-                ////    int rowExcel = 2;
-                ////    for (int i = 0; i < NGrants; i++)
-                ////{
-                ////    workSheet.Cells[rowExcel, "A"] = NamePeople1/*.Rows[i].Cells[0].Value*/;
-                ////        workSheet.Cells[rowExcel, "B"] = NamePeople2/*.Rows[i].Cells[1].Value*/;
-                ////        workSheet.Cells[rowExcel, "C"] = NamePeople3/*.Rows[i].Cells[2].Value*/;
-                ////        workSheet.Cells[rowExcel, "D"] = NamePeople4/*.Rows[i].Cells[4].Value*/;
-                ////        ++rowExcel;
-                ////}
-
-
-
-
-                ////    FileInfo excelFile = new FileInfo("myExcel.xlsx");
-                ////exApp.GetSaveAsFilename(excelFile);
-                ////workSheet.SaveAs("MyExcel.xls");
-
-                //////////////////////////using (ExcelPackage p = new ExcelPackage(FileInfo))
-                ////////////////////////using (var excel = new ExcelPackage())
-                ////////////////////////{
-                ////////////////////////    var ws = excel.Workbook.Worksheets.Add("MyResult");
-
-
-
-                ////////////////////////    ws.Cells["A1"].Value = NamePeople1;
-                ////////////////////////    ws.Cells["B1"].Value = NamePeople2/*g.NumberOfGender*/;
-                ////////////////////////    var _ws = excel.Workbook.Worksheets[1];
-                ////////////////////////    int iRows = _ws.Dimension.Rows;             //считает кол-во строк
-                ////////////////////////    int iColumns = _ws.Dimension.Columns;       //считает кол-во столбцов
-                ////////////////////////    ws.Cells["E1"].Value = iRows;
-                ////////////////////////    ws.Cells["F1"].Value = iColumns;
-
-                ////////////////////////    //ws.Cells["D11"].Value = rowCnt;
-
-                ////////////////////////    excel.SaveAs(new FileInfo("myExcel2.xlsx"));
-                ////////////////////////    //FileInfo excelFile = new FileInfo("myExcel.xlsx");
-                ////////////////////////    //excel.SaveAs(excelFile);
-                ////////////////////////}
-
-                ////создаём новое Excel приложение
-                //Excel.Application exApp = new Excel.Application();
-                ////добавляем рабочую книгу
-                //exApp.Workbooks.Add();
-                ////обращаемся к активному листу (по умолчанию он первый)
-                //Excel.Worksheet workSheet = (Excel.Worksheet)exApp.ActiveSheet;
-                ////добавляем строку в Excel файл
-                //workSheet.Cells[4, 1] = "4.1";
-                ////Вместо чисел можно использовать буквенное обозначение.
-                //workSheet.Cells[1, "B"] = "1.B";
-                //workSheet.Cells[2, 4] = "1.3";
-                ////Сохранение в Excel файл;
-                //workSheet.SaveAs("MyExcel". );
-
-
-                
-
-
-                //FormTest g = (FormTest)Owner;
-                //Form3 m = (Form3)Owner;
-
                 int i = 1;
                 string path = "MyTest.txt";
                 using (StreamReader sr = new StreamReader("MyTest.txt"))
@@ -112,14 +40,8 @@ namespace PsychoApp
                     
                     while ((line = sr.ReadLine()) != null) 
                     {
-                        i++;
-                        
+                        i++; 
                     }
-                    //if ((line = sr.ReadLine()) == null)
-                    //{
-                    //    i = 1;
-                    //}
-                    //i++;
                 }
 
                 using (StreamWriter sw = File.AppendText(path))
@@ -145,9 +67,8 @@ namespace PsychoApp
 
         private void Label1_Click(object sender, EventArgs e)
         {
-            FormTest g = (FormTest)Owner;
-            label1.Text = g.Name_psycho;
-            textBox1.Text = g.Name_psycho;
+            //label1.Text = Name_psycho;
+            textBox1.Text = Name_psycho;
 
         }
     }
